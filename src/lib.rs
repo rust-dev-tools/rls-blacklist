d4fb59bde1699be72496e098ae62975b1c77746b
Nick Cameron <ncameron@mozilla.com>
2018-02-06 13:26:53 +1300

    Add the blacklist
    etc.

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+pub const CRATE_BLACKLIST: [&'static str; 13] = [
+    "libc",
+    "typenum",
+    "alloc",
+    "idna",
+    "openssl",
+    "unicode_normalization",
+    "serde",
+    "serde_json",
+    "rustc_serialize",
+    "unicode_segmentation",
+    "cocoa",
+    "gleam",
+    "winapi",
+];
